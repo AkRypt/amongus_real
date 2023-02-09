e0333a1e59aacf3e8bd8819a3f4af8d5a24b5962
Akshit Dayal <akshitdayal99@gmail.com>
2023-02-09 12:12:32 -0500

    Created production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyheibEWb-trxrovv7lViOAP5-pd9WiOQ",
+    authDomain: "amongusreal-b3a3f.firebaseapp.com",
+    projectId: "amongusreal-b3a3f",
+    storageBucket: "amongusreal-b3a3f.appspot.com",
+    messagingSenderId: "377367621953",
+    appId: "1:377367621953:web:da0f8f35341b6f5e2e31c0"
+  }
 };
